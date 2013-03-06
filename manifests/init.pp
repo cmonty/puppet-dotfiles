82caf0ccf63e7a81a4ec1a02274ec5364ff25686
cory <cory.monty@gmail.com>
2013-03-06 07:59:55 -0600

    removes vim bundles in puppet, favor use directly in my-boxen; adds git config

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,4 @@
 class dotfiles {
-  require vim
-
   $home = "/Users/${::luser}"
   $my = "${home}/my"
   $dotfiles = "${my}/dotfiles"
@@ -18,12 +16,6 @@ class dotfiles {
 
   exec { "cp -r ${dotfiles}/fonts/SourceCodePro ${home}/Library/Fonts/SourceCodePro":
     creates => "${home}/Library/Fonts/SourceCodePro",
-    require => Repository[$dotfiles]
-  }
-
-  file { "${home}/.gitconfig":
-    ensure => "link",
-    target => "${dotfiles}/gitconfig",
     require => Repository[$dotfiles]
   }
 
@@ -45,25 +37,35 @@ class dotfiles {
     require => Repository[$dotfiles]
   }
 
+  git::config::global { 
+    "color.ui": value  => "auto";
+    "alias.ci": value  => "commit";
+    "alias.co": value  => "checkout";
+    "alias.st": value  => "status";
+    "alias.br": value  => "branch";
+    "alias.staged": value  => "diff --staged";
+    "user.name": value => "cory";
+    "user.email": value => "cory.monty@gmail.com";
+    "credential.helper": value => "/opt/boxen/bin/boxen-git-credential";
+    "core.excludesfile": value => "/opt/boxen/config/git/gitignore";
+  }
+
   file { "${home}/.vimrc":
     ensure => "link",
     target => "${dotfiles}/vimrc",
     require => Repository[$dotfiles]
   }
 
+  file { "${home}/.vim":
+    ensure => "directory"
+  }
+
   file { "${home}/.vim/colors":
     ensure => "link",
     target => "${dotfiles}/vim/colors",
-    require => Repository[$dotfiles]
+    require => [
+      File["${home}/.vim"],
+      Repository[$dotfiles]
+    ]
   }
-
-  vim::bundle { "benmills/vimux": }
-  vim::bundle { "c9s/bufexplorer": }
-  vim::bundle { "kien/ctrlp.vim": }
-  vim::bundle { "rodjek/vim-puppet": }
-  vim::bundle { "scrooloose/nerdtree": }
-  vim::bundle { "tomtom/tcomment_vim": }
-  vim::bundle { "tpope/vim-rails": }
-  vim::bundle { "vim-ruby/vim-ruby": }
-  vim::bundle { "vim-scripts/Align": }
 }
